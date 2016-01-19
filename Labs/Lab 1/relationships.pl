0e50d6eaf7218e6fb94ef8658f33c7d1757ce926
Matt Langlois <fletchto99@gmail.com>
2016-01-19 14:28:45 -0500

    Lab 1

---
+++ Labs/Lab 1/relationships.pl
@@ -0,0 +1,31 @@
+city(marie,ottawa).
+city(hong,montreal).
+city(amelie,ottawa). 
+city(paul,kingston).
+city(mohamed,ottawa).
+
+available(marie,saturday). 
+available(marie,sunday). 
+available(hong,saturday).
+available(paul,saturday).
+available(paul,sunday).
+available(mohamed,saturday).
+available(mohamed,sunday).
+available(amelie,sunday).
+
+travel(hong,ottawa)
+travel(hong,kingston).
+travel(mohamed,kingston).
+travel(amelie,montreal).
+
+location(X,Y) :- 
+        city(X,Y).
+location(X,Y) :- 
+        travel(X,Y).
+
+meet(X,Y) :- 
+        location(?,C),
+        location(?,C),
+        X \= Y,
+        available(?,?),
+        available(?,?).
