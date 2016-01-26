e8102adaf818790dc91dd57b126b1194433103b0
Matt Langlois <fletchto99@gmail.com>
2016-01-26 13:09:13 -0500

    DGD 1

--- DGDs/DGD 1/route.pl
+++ DGDs/DGD 1/route.pl
@@ -9,3 +9,7 @@ route('Kinsgston', 'Ottawa',boat).
 % Is there a connection to Quebec City?
 % Print all cities on-route from Toronto to Ottawa.
 % Is there an acceptable connection from Burlington to Ottawa? (For this query a route is acceptable if it is entirely by bus or entirely by boat).
+
+connected(X, Y) :- route(X, Y, _).
+connected(X, Y) :- route(Y, X, _).
+connected(X, Y) :- route(X, Z, _), connected(Z, Y).
