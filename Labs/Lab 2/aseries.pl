bd49f3408b808f7c17fccd05a8c0212ac84bf6c6
Matt Langlois <fletchto99@gmail.com>
2016-01-28 15:08:06 -0500

    Use better variable names to prevent confusion

--- Labs/Lab 2/aseries.pl
+++ Labs/Lab 2/aseries.pl
@@ -1,6 +1,5 @@
 aseries(1, 3).
-aseries(X, Y) :- 
-                X > 1,
-                XX is X - 1,
-                aseries(XX, Z),
+aseries(X, Y) :- X > 1,
+                W is X - 1,
+                aseries(W, Z),
                 (mod(X, 2) =:= 1 -> Y is -5 * Z; Y is 5 * Z).
