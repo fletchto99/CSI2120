343c54b8a250e5295bb31ad708345c94b1384ee2
Matt Langlois <fletchto99@gmail.com>
2016-01-30 13:19:31 -0500

    More assignment 1

--- Assignments/Assignment 1/q2.pl
+++ Assignments/Assignment 1/q2.pl
@@ -1,3 +1,5 @@
-cubeLess(X, B, R) :- R is (B - (X * X * X)), R >= 0.
-smallerCube(X, B) :- var(X), smallerCube(1, B).
-smallerCube(X, B) :- nonvar(X), cubeLess(X, B, _), Xtmp is X+1, smallerCube(Xtmp, B).
+writeCube(X, Y) :-  write(X), write(' rest '), write(Y), nl.
+cubeLess(X, B, R) :- R is (B - (X^3)), R >= 0.
+smallerCube(X, B) :- var(X), smallerCube(1, B), writeCube(X, B).
+smallerCube(X, B) :- nonvar(X), cubeLess(X, B, R), Xtmp is X+1, smallerCube(Xtmp, B), writeCube(X, R).
+%% restSum(X, S) :- smallerCube(X, B), write(B).
