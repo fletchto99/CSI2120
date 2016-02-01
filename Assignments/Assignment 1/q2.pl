d882420475d2833b149fc583c9888dabd93497a2
Matt Langlois <fletchto99@gmail.com>
2016-01-31 23:25:06 -0500

    RestSums!

--- Assignments/Assignment 1/q2.pl
+++ Assignments/Assignment 1/q2.pl
@@ -5,9 +5,7 @@ cubeLess(X, B, R) :- R is (B - (X^3)), R >= 0.
 smallerCube(_, B) :- smallerCubeRecurse(1, B).
 smallerCubeRecurse(X, B) :- cubeLess(X, B, R), Next is X+1, writef('%w rest %w \r\n', [X, R]), smallerCubeRecurse(Next, B).
 
-%% %% C %%
-%% restSum(B, S) :- restSumRecurse(1, B, 0).
-%% restSumRecurse(X, B, S) :- cubeLess(X, B, R), Next is X+1, NextSum is R + S, restSumRecurse(Next, B, NextSum).
-
-restSum(B, S) :- Start is floor(B^(1/3)), restSumRecurse(B, Start, S).
-restSumRecurse(B, Start, S) :- Start > 0, NextStart is Start - 1, cubeLess(Start, B, R), restSumRecurse(B, NextStart, S), S is R.
+%% C %%
+restSum(B, S) :- Start is floor(B^(1/3)), restSum(B, Start, S).
+restSum(_, 0, 0).
+restSum(B, Start, S) :- Start > 0, cubeLess(Start, B, R), NextStart is Start - 1,  restSum(B, NextStart, Sum), S is Sum + R.
