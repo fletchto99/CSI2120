91941594f3fe27feb914c80589b741492ee29a8f
Matt Langlois <fletchto99@gmail.com>
2016-02-23 19:14:16 -0500

    Part 2

--- Assignments/Assignment 2/q1.pl
+++ Assignments/Assignment 2/q1.pl
@@ -1,15 +1,4 @@
-player(ghislain, O, 72) :- !, 
-                        occupation(O).
-
-player(gilles, O, 82) :- !, 
-                    occupation(O).
-
-player(N, O, S) :- person(N), 
-                occupation(O), 
-                score(S).
-
-score(S) :- between(32, 42, N), 
-            S is N * 2.
+% PART A -- Database
 
 person(jules).
 person(gilles).
@@ -22,6 +11,22 @@ occupation(plasterer).
 occupation(carpenter).
 occupation(tinsmith).
 occupation(roofer).
+
+player(ghislain, O, 72) :- !, 
+                        occupation(O).
+
+player(gilles, O, 82) :- !, 
+                    occupation(O).
+
+player(N, O, S) :- person(N), 
+                occupation(O), 
+                score(S).
+
+% Use the built in between predicate to generate numbers, then multiply by 2 to get only the even numbers
+score(S) :- between(32, 42, N), 
+            S is N * 2.
+
+% PART B -- Constraints
 
 constraintRoofer(player(N, roofer, 74)) :- N \= ghislain, 
                                         !.
@@ -45,6 +50,8 @@ constraintCarpenter(player(N, carpenter, S), player(jules, O, Js)) :- N \= jules
                                                                     !.
 constraintCarpenter(player(_, O, _), player(_, _, _)) :- O \= carpenter.
 
+
+% Create the constraints to be a winner, due to the previous constraints given we can immediatley eliminate some names
 constraintWin(player(N, tinsmith, S), S1, S2, S3, S4) :- N \= jules, 
                                         N \= joe,
                                         N \= ghislain,
@@ -57,15 +64,17 @@ constraintWin(player(N, tinsmith, S), S1, S2, S3, S4) :- N \= jules,
                                         !.
 constraintWin(player(_, O, _), _, _, _, _) :- O \= tinsmith.
 
-%%scores(player(jules,M1,P1), player(jean,M2,P2), player(gilles,M3,P3), player(joe,M4,P4), player(ghislain,M5,P5)). 
+% PART C -- Scores predicate
 
 scores(player(N1, O1, S1), player(N2, O2, S2), player(N3, O3, S3), player(N4, O4, S4), player(N5, O5, S5)):-
+    % Generate all possible combinations of scores and occupations for each player
     player(N1, O1, S1),
     player(N2, O2, S2),
     player(N3, O3, S3),
     player(N4, O4, S4),
     player(N5, O5, S5),
 
+    % Ensure none of the players have the same occupation, since occupation -> people is a 1-1 relationship
     O1 \= O2,
     O1 \= O3,
     O1 \= O4,
@@ -77,30 +86,38 @@ scores(player(N1, O1, S1), player(N2, O2, S2), player(N3, O3, S3), player(N4, O4
     O3 \= O5,
     O4 \= O5,
 
+    % Check the roofer constraint for each of the players
     constraintRoofer(player(N1, O1, S1)),
     constraintRoofer(player(N2, O2, S2)),
     constraintRoofer(player(N3, O3, S3)),
     constraintRoofer(player(N4, O4, S4)),
     constraintRoofer(player(N5, O5, S5)),
 
+    % Check the mason constraint for each player
     constraintMason(player(N1, O1, S1)),
     constraintMason(player(N2, O2, S2)),
     constraintMason(player(N3, O3, S3)),
     constraintMason(player(N4, O4, S4)),
     constraintMason(player(N5, O5, S5)),
 
+    % check the tinsmith constraint for each player, we know the second player parameter will be joe due to the constraints
     constraintTinsmith(player(N1, O1, S1), player(joe, O4, S4)),
     constraintTinsmith(player(N2, O2, S2), player(joe, O4, S4)),
     constraintTinsmith(player(N3, O3, S3), player(joe, O4, S4)),
     constraintTinsmith(player(N5, O5, S5), player(joe, O4, S4)),
 
+    % check the carpenter constraint for each player, we know the second player parameter will be jules due to the constraints
     constraintCarpenter(player(N2, O2, S2), player(jules, O1, S1)),
     constraintCarpenter(player(N3, O3, S3), player(jules, O1, S1)),
     constraintCarpenter(player(N4, O4, S4), player(jules, O1, S1)),
     constraintCarpenter(player(N5, O5, S5), player(jules, O1, S1)),
 
+    % Check the win constrain for every possible player
     constraintWin(player(N1, O1, S1), S2, S3, S4, S5),
     constraintWin(player(N2, O2, S2), S1, S3, S4, S5),
     constraintWin(player(N3, O3, S3), S1, S2, S4, S5),
     constraintWin(player(N4, O4, S4), S1, S2, S3, S5),
-    constraintWin(player(N5, O5, S5), S1, S2, S3, S4).
+    constraintWin(player(N5, O5, S5), S1, S2, S3, S4),
+
+    % Cut since we know only one solution is possible
+    !.
--- Assignments/Assignment 2/q2.pl
+++ Assignments/Assignment 2/q2.pl
@@ -1,15 +1,13 @@
-test(X) :- tree(X), lca(83, 2016, X). 
-
-tree(X):- X = t (73, 
-                    t (31, 
+tree(X) :- X = t(73,
+                    t(31, 
                         t(5,
                             nil,
                             nil
                         ), 
                         nil
                     ), 
-                    t (101, 
-                        t (83, 
+                    t(101, 
+                        t(83, 
                             nil, 
                             t(97,
                                 nil,
@@ -21,6 +19,15 @@ tree(X):- X = t (73,
                             nil
                         )
                     )
-                ). 
+                ).
 
-lca(K1, K2, T) :-
+% Predicate to initiate recursion, ensures parameters are in right order
+lca(K1, K2, T) :- K1 > K2, lcaRecurse(K2, K1, T), !.
+lca(K1, K2, T) :- K2 > K1, lcaRecurse(K1, K2, T), !.
+
+% Base case
+lcaRecurse(K1, K2, t(Current, Left, Right)) :- Current >= K1, Current =< K2, write(t(Current, Left, Right)), !.
+
+% Recursive case for both left and right nodes
+lcaRecurse(K1, K2, t(Current, Left, _)) :- Current > K2, lcaRecurse(K1, K2, Left), !.
+lcaRecurse(K1, K2, t(Current, _, Right)) :- Current < K1, lcaRecurse(K1, K2, Right), !.
