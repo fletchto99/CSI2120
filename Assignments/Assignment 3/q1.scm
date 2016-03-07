a8cbf338395932ce04c92b94a3acc200864afb94
Matt Langlois <fletchto99@gmail.com>
2016-03-07 16:26:01 -0500

    Use built in append predicate

--- Assignments/Assignment 3/q1.scm
+++ Assignments/Assignment 3/q1.scm
@@ -1,42 +1,23 @@
 (define (sum sumList) (
         if (null? sumList)
             0
-            (+ (car sumList) (sum (cdr sumList)))
-    )
-)
-
-(define (apply-list function L) (
-        if (null? L)
-            '()
-            (cons (function (car L)) (apply-list function (cdr L)))
-    )
-)
+            (+ (car sumList) (sum (cdr sumList)))))
 
 (define (normalizeRecurse filter sum) (
-        apply-list (lambda(x) (/ x sum)) filter
-    )
-)
+        map (lambda(x) (/ x sum)) filter))
 
 (define (normalize filter) (
-        normalizeRecurse filter (sum filter)
-    )
-)
+        normalizeRecurse filter (sum filter)))
 
 (define (calculate filter results) (
         cond
         ((null? filter) 0)
         ((null? results) 0)
-        (else (+ (* (car filter) (car results)) (calculate (cdr filter) (cdr results))))
-    )
-)
-
-;(calculate (normalize '(1 2 3 4)) '(1.0 1.0 2.0 3.0 5.0 8.0 13.0)) 
+        (else (+ (* (car filter) (car results)) (calculate (cdr filter) (cdr results))))))
 
 (define (filter filterList resultsList) (
         if (null? resultsList)
             '()
-            (cons (calculate (normalize filterList) resultsList) (filter filterList (cdr resultsList)))
-    )
-)
+            (cons (calculate (normalize filterList) resultsList) (filter filterList (cdr resultsList)))))
 
 (filter '(1 2 5 2 1) '(0.1 0.1 0.1 0.7 0.1 0.2 0.3 0.4 0.5 0.6 0.7)) 
