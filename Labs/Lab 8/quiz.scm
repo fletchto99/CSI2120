7f915c0442a65ee2f81688e4b870838c1496893b
Matt Langlois <fletchto99@gmail.com>
2016-03-18 00:23:45 -0400

    Lab 8 Quiz

--- Labs/Lab 8/quiz.scm
+++ Labs/Lab 8/quiz.scm
@@ -1,6 +1,8 @@
-;   Letter removal: Removes all occurence of a letter from the string
-;     
-;       Example:
-;   (removeLetter "Hello World" #\o)
-;   => "Hell Wrld"
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (removeLetterRecurse lst char acc) (
+    cond 
+    ((null? lst) (list->string acc))
+    ((char=? (car lst) char) (removeLetterRecurse (cdr lst) char acc))
+    (else (removeLetterRecurse (cdr lst) char (append acc (string->list(string (car lst))))))))
+
+(define (removeLetter str char) (removeLetterRecurse (string->list str) char '()))
+(removeLetter "Hello World" #\o)
